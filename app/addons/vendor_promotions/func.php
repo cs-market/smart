871dabe45ab78b88f645982f1ef12bff27836b95
bizon <bizon.ru@gmail.com>
2019-09-03 12:39:38 +0300

    Revert "vendor promotion availability"
    This reverts commit d6aad1ea748bd40de3d10083c2488a39d82dffe7.

--- app/addons/vendor_promotions/func.php
+++ app/addons/vendor_promotions/func.php
@@ -1,13 +1,5 @@
 <?php
 
-use Tygh\Registry;
-
 function fn_vendor_promotions_get_promotion_data_pre($promotion_id, &$extra_condition, $lang_code) {
 	$extra_condition .= fn_get_company_condition('p.company_id');
-}
-
-function fn_vendor_promotions_update_promotion_pre(&$data, $promotion_id, $lang_code) {
-	if (Registry::get('runtime.company_id')) {
-		$data['company_id'] = Registry::get('runtime.company_id');
-	}
 }
--- app/addons/vendor_promotions/init.php
+++ app/addons/vendor_promotions/init.php
@@ -1,6 +1,5 @@
 <?php
 
 fn_register_hooks(
-    'get_promotion_data_pre',
-    'update_promotion_pre'
+    'get_promotion_data_pre'
 );
