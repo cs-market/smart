0abf1368227b476731a5002e0724a8cb48070599
bizon <bizon.ru@gmail.com>
2020-02-04 13:47:33 +0300

    + invoice for payment add-on

---
+++ app/addons/invoice_for_payment/payments/invoice_payment.php
@@ -0,0 +1,16 @@
+<?php
+
+if (defined('PAYMENT_NOTIFICATION')) {
+
+} else {
+
+	if (!empty($processor_data['processor_params']['account_order_status'])) {
+		$pp_response = array(
+			'order_status' => $processor_data['processor_params']['account_order_status']
+		);
+	} else {
+		$pp_response = array(
+			'order_status' => 'O'
+		);
+	}
+}
