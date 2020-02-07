6a4a5405985e68add7c67bdbb9542c1b02787186
bizon <bizon.ru@gmail.com>
2020-02-07 12:55:41 +0300

    ! user price import: import by login first

--- app/addons/user_price/schemas/exim/user_price.functions.php
+++ app/addons/user_price/schemas/exim/user_price.functions.php
@@ -2,7 +2,6 @@
 
 function fn_import_user_price(&$primary_object_id, &$object, &$options, &$processed_data, &$processing_groups) {
 	$name = trim($object['Name']);
-	//$ug_id = db_get_field('SELECT usergroup_id FROM ?:usergroup_descriptions WHERE usergroup = ?s AND lang_code = ?s', $name, $object['lang_code']);
 	if ($ug_id = db_get_field('SELECT usergroup_id FROM ?:usergroup_descriptions WHERE usergroup = ?s AND lang_code = ?s', $name, $object['lang_code'])) {
 		$price = array(
 			'product_id' => $primary_object_id['product_id'],
@@ -15,17 +14,23 @@ function fn_import_user_price(&$primary_object_id, &$object, &$options, &$proces
 		//process_qty_discounts
 	} else {
 		$like_name = '%' . $name . '%';
-		//get user_id by function!!
-		$price = array(
-			'user_id' => db_get_field('SELECT user_id FROM ?:users WHERE firstname LIKE ?l OR lastname LIKE ?l', $like_name, $like_name),
-			'price' => $object['price'],
-		);
-		if ($price['user_id']) {
+		//TODO make fn_get_users more smart!
+		list($users, ) = fn_get_users(array('user_login' => $name));
+		if (!empty($users)) {
+			$user = array_shift($users);
+		} else {
+			list($users, ) = fn_get_users(array('name' => $name));
+			$user = array_shift($users);
+		}
+		if (!empty($user)) {
+			$price = array(
+				'user_id' => $user['user_id'],
+				'price' => $object['price'],
+			);
 			//process_user_prices
 			if (fn_update_product_user_price($primary_object_id['product_id'], array($price), false)) {
 				$processed_data['E'] += 1;
 			}
-
 		} else {
 			// skip record
 			$processed_data['S'] += 1;
