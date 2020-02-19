d125fd348b9303690e233bf49be6ad0bcf598c9d
WaliterIrlandec <irek.khalimov@gmail.com>
2020-02-19 19:29:16 +0300

    pay_by_points temp commit

--- app/addons/pay_by_points/controllers/frontend/checkout.post.php
+++ app/addons/pay_by_points/controllers/frontend/checkout.post.php
@@ -20,19 +20,8 @@ if ($mode == 'point_payment_product') {
 
   fn_add_product_to_cart([$cart_id => $product_data], $cart, $auth);
   fn_calculate_cart_content($cart, $auth, 'S', true);
-  // 'E'
-  fn_print_r('s');
-  fn_print_die("z");
-  // FIXME: mb call calculate cart if it !call auto
-  $cart['recalculate'] = true;
-$product_price = $cart['total'];
-unset($cart);
-  $points_to_use = empty($_REQUEST['points_to_use']) ? 0 : intval($_REQUEST['points_to_use']);
-  if (!empty($points_to_use) && abs($points_to_use) == $points_to_use) {
-    Tygh::$app['session']['cart']['points_info']['in_use']['points'] = $points_to_use;
-  }
 
-  $redirect_mode = isset($_REQUEST['redirect_mode']) ? $_REQUEST['redirect_mode'] : 'checkout';
+  $redirect_mode = isset($_REQUEST['redirect_mode']) ? $_REQUEST['redirect_mode'] : 'cart';
 
   return array(CONTROLLER_STATUS_REDIRECT, 'checkout.' . $redirect_mode . '.show_payment_options');
 }
