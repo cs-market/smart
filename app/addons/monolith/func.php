3072bd017947765bac769d819d5c9ac48e9e07a3
bizon <bizon.ru@gmail.com>
2020-02-28 18:24:21 +0400

    [+] Monolith add-on

---
+++ app/addons/monolith/func.php
@@ -0,0 +1,84 @@
+<?php
+
+use Tygh\Registry;
+use Tygh\Http;
+
+if (!defined('BOOTSTRAP')) { die('Access denied'); }
+
+function fn_monolith_place_order($order_id, $action, $order_status, $cart, $auth) {
+	$schema = fn_get_schema('monolith', 'schema');
+	$addon = Registry::get('addons.monolith');
+	$allowed_companies = explode(',', $addon['company_ids']);
+
+	$order_info = fn_get_order_info($order_id);
+	if (!in_array($order_info['company_id'], $allowed_companies)) {
+		return false;
+	}
+
+	$monolith_order = &$schema['extdata']['scheme']['data']['o'];
+	$monolith_order['d'][] = array(
+		'@attributes' => array('name' => 'CRMOrderParam'),
+		'r' => array(
+			'f' => array(
+				date("Y-m-d\TH:i:s", $order_info['timestamp']),
+				1,
+				1,
+				date("Y-m-d\TH:i:s", $order_info['timestamp']),
+			),
+		),
+	);
+
+	$monolith_order['d'][] = array(
+		'@attributes' => array ('name' => 'CRMOrder'),
+		'r' => array(
+			'f' => array(
+				$addon['order_prefix'] . $order_id,
+				date("Y-m-d\TH:i:s", $order_info['timestamp']),
+				$order_info['fields']['38'], 
+				$order_info['fields']['38'],
+				'',
+				$order_info['user_id'],
+				date("Y-m-d\TH:i:s", $order_info['timestamp']),
+				'CustOrder',
+				'Entered',
+			),
+		),
+	);
+
+	$iterator = 0;
+	foreach ($order_info['products'] as $p) {
+		$iterator += 1;
+		$monolith_order['d'][] = array(
+			'@attributes' => array ('name' => 'CRMOrderLine'),
+			'r' => array(
+				'f' => array(
+					$addon['order_prefix'] . $order_id,
+					$iterator,
+					'smart.wh1',
+					'',
+					$p['product_code'],
+					'ea',
+					$p['price'],
+					$p['amount']
+				),
+			),
+		);
+	}
+
+	// if we have comment
+	if (!empty($order_info['notes'])) {
+		$monolith_order['d'][] = array(
+			'@attributes' => array ('name' => 'CRMOrderOption'),
+			'r' => array(
+				'f' => array(
+					$addon['order_prefix'] . $order_id, 'Comment', $order_info['notes'],
+				)
+			),
+		);
+	} else {
+		unset($schema['extdata']['scheme']['data']['s']['d'][3]);
+	}
+
+	$xml = fn_render_xml_from_array($schema);
+	$result = HTTP::POST($addon['environment_url'], array('XMLData'=>$xml));
+}
---
+++ app/addons/monolith/schemas/monolith/schema.php
@@ -0,0 +1,202 @@
+<?php 
+
+use Tygh\Registry;
+
+$addon = Registry::get('addons.monolith');
+
+$schema = array(
+    'extdata' => array (
+        '@attributes' => array (
+            'user' => $addon['gate_code'],
+        ),
+        'scheme' => array (
+            '@attributes' => array (
+                'name' => 'CRMOrderExtSet',
+                'request' => 'set'
+            ),
+            'data' => array (
+                's' => array (
+                    'd' => array (
+                        '0' => array (
+                            '@attributes' => array (
+                                'name' => 'CRMOrderParam',
+                            ),
+                            'f' => array (
+                                '0' => array (
+                                    '@attributes' => array (
+                                        'name' => 'WorkDate',
+                                        'type' => 'Date',
+                                    ),
+                                ),
+                               '1' => array (
+                                    '@attributes' => array (
+                                        'name' => 'SkipDelete',
+                                        'type' => 'Integer',
+                                    ),
+                                ),
+                                '2' => array (
+                                    '@attributes' => array (
+                                        'name' => 'DeleteWHIncludedOnly',
+                                        'type' => 'Integer',
+                                    ),
+                                ),
+                                '3' => array (
+                                    '@attributes' => array (
+                                        'name' => 'WorkPeriodEnd',
+                                        'type' => 'Date',
+                                    ),
+                                ),
+                            ),
+                        ),
+                        '1' => array (
+                            '@attributes' => array (
+                                'name' => 'CRMOrder',
+                            ),
+                            'f' => array (
+                                '0' => array (
+                                    '@attributes' => array (
+                                        'name' => 'CRMOrderNumber',
+                                        'type' => 'String',
+                                    ),
+                                ),
+                                '1' => array (
+                                    '@attributes' => array (
+                                        'name' => 'CRMOrderDate',
+                                        'type' => 'Date',
+                                    ),
+                                ),
+                                '2' => array (
+                                    '@attributes' => array (
+                                        'name' => 'CompanyId',
+                                        'type' => 'String',
+                                    ),
+                                ),
+                                '3' => array (
+                                    '@attributes' => array (
+                                        'name' => 'AddressId',
+                                        'type' => 'String',
+                                    ),
+                                ),
+                                '4' => array (
+                                    '@attributes' => array (
+                                        'name' => 'WareHouseId',
+                                        'type' => 'String',
+                                    ),
+                                ),
+                                '5' => array (
+                                    '@attributes' => array (
+                                        'name' => 'PersonId',
+                                        'type' => 'String',
+                                    ),
+                                ),
+                                '6' => array (
+                                    '@attributes' => array (
+                                        'name' => 'ActionDate',
+                                        'type' => 'Date',
+                                    ),
+                                ),
+                                '7' => array (
+                                    '@attributes' => array (
+                                        'name' => 'DocumentTypeId',
+                                        'type' => 'String',
+                                    ),
+                                ),
+                                '8' => array (
+                                    '@attributes' => array (
+                                        'name' => 'StatusId',
+                                        'type' => 'String',
+                                    ),
+                                ),
+                            ),
+                        ),
+                        '2' => array (
+                            '@attributes' => array (
+                                'name' => 'CRMOrderLine',
+                            ),
+                            'f' => array (
+                                '0' => array (
+                                    '@attributes' => array (
+                                        'name' => 'CRMOrderNumber',
+                                        'type' => 'String',
+                                    ),
+                                ),
+                                '1' => array (
+                                    '@attributes' => array (
+                                        'name' => 'LineNumber',
+                                        'type' => 'Integer',
+                                    ),
+                                ),
+                                '2' => array (
+                                    '@attributes' => array (
+                                        'name' => 'CRMWareHouseId',
+                                        'type' => 'String',
+                                    ),
+                                ),
+                                '3' => array (
+                                    '@attributes' => array (
+                                        'name' => 'WareHouseId',
+                                        'type' => 'String',
+                                    ),
+                                ),
+                                '4' => array (
+                                    '@attributes' => array (
+                                        'name' => 'WareId',
+                                        'type' => 'String',
+                                    ),
+                                ),
+                                '5' => array (
+                                    '@attributes' => array (
+                                        'name' => 'UnitId',
+                                        'type' => 'String',
+                                    ),
+                                ),
+                                '6' => array (
+                                    '@attributes' => array (
+                                        'name' => 'Price',
+                                        'type' => 'Currency',
+                                    ),
+                                ),
+                                '7' => array (
+                                    '@attributes' => array (
+                                        'name' => 'Quantity',
+                                        'type' => 'Currency',
+                                    ),
+                                ),
+                            ),
+                        ),
+                        '3' => array (
+                            '@attributes' => array (
+                                'name' => 'CRMOrderOption',
+                            ),
+                            'f' => array (
+                                '0' => array (
+                                    '@attributes' => array (
+                                        'name' => 'CRMOrderNumber',
+                                        'type' => 'String',
+                                    ),
+                                ),
+                                '1' => array (
+                                    '@attributes' => array (
+                                        'name' => 'OptionTypeId',
+                                        'type' => 'String',
+                                    ),
+                                ),
+                                '2' => array (
+                                    '@attributes' => array (
+                                        'name' => 'Value',
+                                        'type' => 'String',
+                                    ),
+                                ),
+                            ),
+                        ),
+                    ),
+                ),
+                'o' => array (
+
+                )
+            )
+        )
+    )
+);
+
+return $schema;
