2673bf50a469d53d7402a7962267e46661d129e5
WaliterIrlandec <irek.khalimov@gmail.com>
2020-03-23 02:14:08 +0300

    [+] add the add-on user_from_form

---
+++ app/addons/user_from_form/func.php
@@ -0,0 +1,66 @@
+<?php
+
+// use Tygh\Registry;
+
+//  [HOOKs]
+function fn_user_from_form_send_form($page_data, $form_values, $result, $from, $sender, $attachments, $is_html, $subject)
+{
+  if ($result != true) {
+    return;
+  }
+
+  $schema = fn_get_schema('user_from_form', 'schema');
+
+  if (!in_array($page_data['page_id'], array_keys($schema))) {
+    return;
+  }
+
+  $form_schema = $schema[$page_data['page_id']];
+
+  $user_data = fn_create_user_field_from_form($form_schema['fields_map'], $form_values);
+
+  $user_data = array_merge(
+    $user_data,
+    [
+      'user_type' => 'C',
+      'company_id' => $form_schema['company_id'],
+    ]
+  );
+
+  list($user_id) = fn_update_user(
+      '',
+      $user_data,
+      $auth,
+      false,   // ship_to_another
+      false   // notify_customer
+  );
+
+  if ($user_id && $form_schema['usergroup_id']) {
+    foreach ($form_schema['usergroup_id'] as $id) {
+      //  main usergroups
+      if (in_array($id, ['0', '1'])) {
+        continue;
+      }
+
+      fn_change_usergroup_status('A', $user_id, $id);
+    }
+  }
+}
+//  [HOOKs]
+
+function fn_create_user_field_from_form($fields, $values)
+{
+  $field_data = [];
+
+  foreach ($fields as $key => $name) {
+    if (is_array($name)) {
+      foreach ($name as $n) {
+        $field_data[$n] = $values[$key];
+      }
+    } else {
+      $field_data[$name] = $values[$key];
+    }
+  }
+
+  return $field_data;
+}
---
+++ app/addons/user_from_form/schemas/user_from_form/schema.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+  '57' => [
+    'page_id' => 57,
+    'company_id' => 16,
+    'usergroup_id' => [9, 27],
+    'fields_map' => [
+      '57' => 'firstname',
+      '58' => 'password',
+      '59' => 'phone',
+      '60' => ['email', 'login'],
+      '61' => 'address',
+    ],
+  ],
+  '56' => [
+    'page_id' => 56,
+    'company_id' => 16,
+    'usergroup_id' => [1],
+    'fields_map' => [
+      '56' => 'firstname',
+      // '58' => 'password',
+        '49' => ['email', 'login'],
+      '44' => 'b_firstname',
+      '45' => 'b_address',
+      '47' => 'fax',
+    ],
+  ],
+];
