61b26bce3b36b4e01afb22c1b66829ee2cdbef1d
Alex <stoprocentov666@gmail.com>
2020-07-31 16:18:19 +0400

    [+] Add promo_text and promotion products block

--- app/addons/category_promotion/init.php
+++ app/addons/category_promotion/init.php
@@ -3,9 +3,10 @@
 if (!defined('BOOTSTRAP')) { die('Access denied'); }
 
 fn_register_hooks(
-	'get_products_before_select',
-	'get_products',
-	'update_promotion_post',
-	'get_autostickers_pre',
-	'get_promotions'
+    'get_products_before_select',
+    'get_products',
+    'update_promotion_post',
+    'get_autostickers_pre',
+    'get_promotions',
+    'get_product_data_post'
 );
