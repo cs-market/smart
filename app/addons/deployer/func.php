448bc080b39e58ac1281e2a66a38d913d20a3fcc
bizon <bizon.ru@gmail.com>
2020-12-30 19:27:25 +0300

    [*] deployer add-on

--- app/addons/deployer/func.php
+++ app/addons/deployer/func.php
@@ -4,7 +4,6 @@ use Tygh\Registry;
 use Tygh\Settings;
 use Tygh\UpgradeCenter\Migrations\Migration;
 use Tygh\Tools\SecurityHelper;
-use Tygh\Validators;
 
 if (!defined('BOOTSTRAP')) { die('Access denied'); }
 
@@ -24,21 +23,25 @@ function fn_deploy($webhook) {
         $branch = isset( $lastChange['name'] ) && ! empty( $lastChange['name'] ) ? $lastChange['name'] : '';
         $addon = Registry::get('addons.deployer');
         if ($branch = $addon['branch']) {
+            $current_dir = getcwd();
             if ($addon['reset']) {
+                chdir($addon['git_path']);
                 exec('git reset --hard HEAD', $output);
+                chdir($current_dir);
                 fn_write_deploy_log(reset($output));
             }
             if (!empty(trim($addon['migrations_path']))) {
-                $old_migrations['php'] = fn_get_dir_contents($addon['migrations_path'], false, true, 'php');
-                $old_migrations['sql'] = fn_get_dir_contents($addon['migrations_path'], false, true, array('.sql', '.tgz', '.zip'));
+                $old_migrations = fn_get_dir_contents($addon['migrations_path'], false, true, 'php');
             }
+
+            chdir($addon['git_path']);
             exec('git pull ' . $addon['remote'] . ' ' . $addon['branch'], $output);
+            chdir($current_dir);
             fn_write_deploy_log('result: ' . reset($output));
 
-            // apply phinx migrations
             if (!empty(trim($addon['migrations_path']))) {
-                $current_migrations['php'] = fn_get_dir_contents($addon['migrations_path'], false, true, 'php');
-                $new_migrations = array_diff($current_migrations, $old_migrations['php']);
+                $current_migrations = fn_get_dir_contents($addon['migrations_path'], false, true, 'php');
+                $new_migrations = array_diff($current_migrations, $old_migrations);
                 if (!empty($new_migrations)) {
                     fn_mkdir($addon['migrations_path'] . 'run/');
                     foreach ($new_migrations as $migration_file) {
@@ -65,27 +68,6 @@ function fn_deploy($webhook) {
                 }
             }
 
-            //apply zip and sql backups
-            fn_mkdir($addon['migrations_path'] . 'run/');
-            $sql_files = fn_get_dir_contents($addon['migrations_path'], false, true, array('.sql', '.tgz', '.zip'));
-            $new_sql_files = array_diff($sql_files, $old_migrations['sql']);
-            foreach ($new_sql_files as $file) {
-                $ext = fn_get_file_ext($addon['migrations_path'] . $file);
-
-                if ($ext == 'tgz' && !$validators->isPharDataAvailable()) {
-                    continue;
-                }
-                if ($ext == 'zip' && !$validators->isZipArchiveAvailable()) {
-                    continue;
-                }
-                $restore_result = DataKeeper::restore($file);
-                if ($restore_result === true) {
-                    fn_write_deploy_log(__('done') . ': ' . $file);
-                } else {
-                    fn_write_deploy_log(__('error_occured') . ': ' . $file);
-                }
-            }
-
             fn_clear_cache();
             fn_clear_template_cache();
         }
@@ -96,6 +78,12 @@ function fn_deployer_install() {
     Settings::instance()->updateValue(
         'token',
         SecurityHelper::generateRandomString(),
+        'deployer'
+    );
+
+    Settings::instance()->updateValue(
+        'git_path',
+        DIR_ROOT,
         'deployer'
     );
 }
