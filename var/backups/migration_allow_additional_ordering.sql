dd714f3987561f77effa39f54f06a2b981df6a12
bizon <bizon.ru@gmail.com>
2021-04-14 18:06:25 +0300

    [+] remove old migrations

--- var/backups/migration_allow_additional_ordering.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `cscart_companies` ADD `allow_additional_ordering` char(1) NOT NULL DEFAULT 'N' after `min_order_amount`;
--- var/backups/migration_show_out_of_stock_product.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `cscart_products` ADD `show_out_of_stock_product` VARCHAR(1) NOT NULL DEFAULT 'Y';
