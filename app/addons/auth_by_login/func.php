a60e079821f5ab00a58f54f0b40818fd6d7bb50c
bizon <bizon.ru@gmail.com>
2021-04-23 17:21:06 +0300

    [!] fix in auth by new passwords.

--- app/addons/auth_by_login/func.php
+++ app/addons/auth_by_login/func.php
@@ -5,32 +5,25 @@ use Tygh\Registry;
 if ( !defined('AREA') ) { die('Access denied'); }
 
 function fn_auth_by_login_auth_routines($request, $auth, &$field, &$condition, &$user_login) {
-	$users_data = db_get_array("SELECT * FROM ?:users WHERE (email = ?s OR user_login = ?s)" . $condition, $user_login, $user_login);
+    $users_data = db_get_array("SELECT * FROM ?:users WHERE (email = ?s OR user_login = ?s)" . $condition, $user_login, $user_login);
 
-	if (!empty($users_data)) {
-		foreach ($users_data as $offset => $user_data) {
-			$password = (!empty($request['password'])) ? $request['password']: '';
-			$salt = isset($user_data['salt']) ? $user_data['salt'] : '';
-			if (fn_generate_salted_password($password, $salt) == $user_data['password']) {
-				break;
-			}
-		}
-
-		if (!empty($user_data)) {
-			$field = '1';
-			$user_login = '1';
-			$condition .= db_quote(" AND user_id = ?i", $user_data['user_id']);
-		}
-	}
-}
-
-function fn_auth_by_login_is_user_exists_pre($user_id, &$user_data) {
-	//unset($user_data['email'], $user_data['user_login']);
+    if (!empty($users_data) && count($users_data) > 1) {
+        foreach ($users_data as $user_data) {
+            $password = (!empty($request['password'])) ? $request['password']: '';
+            $salt = isset($user_data['salt']) ? $user_data['salt'] : '';
+            if (fn_user_password_verify((int) $user_data['user_id'], $password, (string) $user_data['password'], (string) $salt)) {
+                $field = '1';
+                $user_login = '1';
+                $condition .= db_quote(" AND user_id = ?i", $user_data['user_id']);
+                break;
+            }
+        }
+    }
 }
 
 function fn_auth_by_login_user_exist($user_id, $user_data, &$condition) {
-	if (!empty($user_data['company_id']) && !empty($user_data['user_login'])) {
-		$condition = db_quote(' user_login = ?s AND company_id = ?i', $user_data['user_login'], $user_data['company_id']);
-		$condition .= db_quote(" AND user_id != ?i", $user_id);
-	}
+    if (!empty($user_data['company_id']) && !empty($user_data['user_login'])) {
+        $condition = db_quote(' user_login = ?s AND company_id = ?i', $user_data['user_login'], $user_data['company_id']);
+        $condition .= db_quote(" AND user_id != ?i", $user_id);
+    }
 }
--- app/addons/auth_by_login/init.php
+++ app/addons/auth_by_login/init.php
@@ -3,7 +3,6 @@
 if (!defined('BOOTSTRAP')) { die('Access denied'); }
 
 fn_register_hooks(
-	'auth_routines',
-	'is_user_exists_pre',
-	'user_exist'
+    'auth_routines',
+    'user_exist'
 );
