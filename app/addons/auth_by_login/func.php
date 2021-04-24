ffabc1d9c89c2b57b4c9c290297a3ded52f9601b
bizon <bizon.ru@gmail.com>
2021-04-24 18:31:04 +0300

    [!] auth should be by email = OR user_login at any case

--- app/addons/auth_by_login/func.php
+++ app/addons/auth_by_login/func.php
@@ -7,7 +7,7 @@ if ( !defined('AREA') ) { die('Access denied'); }
 function fn_auth_by_login_auth_routines($request, $auth, &$field, &$condition, &$user_login) {
     $users_data = db_get_array("SELECT * FROM ?:users WHERE (email = ?s OR user_login = ?s)" . $condition, $user_login, $user_login);
 
-    if (!empty($users_data) && count($users_data) > 1) {
+    if (!empty($users_data)) {
         foreach ($users_data as $user_data) {
             $password = (!empty($request['password'])) ? $request['password']: '';
             $salt = isset($user_data['salt']) ? $user_data['salt'] : '';
