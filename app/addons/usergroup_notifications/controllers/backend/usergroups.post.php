89e3d4803484cc86ded9aa7c5d759fba23825fef
bizon <bizon.ru@gmail.com>
2021-05-19 13:47:45 +0300

    [!] Fix to save email with Usergroup notifications addon

--- app/addons/usergroup_notifications/controllers/backend/usergroups.post.php
+++ app/addons/usergroup_notifications/controllers/backend/usergroups.post.php
@@ -10,7 +10,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if ($mode == 'update') {
         $usergroup_id = $_REQUEST['usergroup_id'] ?? 0;
         if ($usergroup_id) {
-            db_query('UPDATE ?:usergroup_descriptions SET email_template = ?s', $_REQUEST['usergroup_data']['email_template']);
+            db_query('UPDATE ?:usergroup_descriptions SET email_template = ?s WHERE usergroup_id = ?i AND lang_code = ?s', $_REQUEST['usergroup_data']['email_template'], $usergroup_id, DESCR_SL);
         }
     }
 
