752dec345db4ec87d24d09f38763a7957d33bc0f
bizon <bizon.ru@gmail.com>
2021-06-24 21:21:09 +0300

    [*] Exim: Date column should be without time

--- app/addons/smart_distribution/schemas/exim/orders.post.php
+++ app/addons/smart_distribution/schemas/exim/orders.post.php
@@ -5,13 +5,13 @@ use Tygh\Registry;
 include_once(Registry::get('config.dir.addons') . 'smart_distribution/schemas/exim/orders.functions.php');
 
 $schema['export_fields']['1c'] = array(
-	'linked' => false,
-	'process_get' => array('fn_get_1c_code', '#key')
+    'linked' => false,
+    'process_get' => array('fn_get_1c_code', '#key')
 );
 
 $schema['export_fields']['Payment method'] = array(
-	'db_field' => 'payment_id',
-	'process_get' => array('fn_get_payment_name', '#this', '#lang_code')
+    'db_field' => 'payment_id',
+    'process_get' => array('fn_get_payment_name', '#this', '#lang_code')
 );
 
 $schema['export_fields']['E-mail']['required'] = false;
@@ -21,5 +21,7 @@ $schema['import_process_data']['change_order_status'] = array(
     'args' => array('$object'),
     'import_only' => true,
 );
+
+$schema['export_fields']['Date']['process_get'][0] = 'fn_timestamp_to_date_wo_time';
 
 return $schema;
