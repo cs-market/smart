bd64341463bf0adff508b41fb31bc70016ca2b70
bizon <bizon.ru@gmail.com>
2021-12-29 22:14:18 +0300

    [*] new design

---
+++ app/addons/rumba/schemas/block_manager/blocks.post.php
@@ -0,0 +1,23 @@
+<?php
+/** * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+*      Copyright (c) 2013 CS-Market Ltd. All rights reserved.             *
+*                                                                         *
+*  This is commercial software, only users who have purchased a valid     *
+*  license and accept to the terms of the License Agreement can install   *
+*  and use this program.                                                  *
+*                                                                         *
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+*  PLEASE READ THE FULL TEXT OF THE SOFTWARE LICENSE AGREEMENT IN THE     *
+*  "license agreement.txt" FILE PROVIDED WITH THIS DISTRIBUTION PACKAGE.  *
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * **/
+
+$schema['banners']['templates']['addons/rumba/blocks/grid.tpl'] = [
+    'settings' => array(
+        'number_of_columns' =>  array (
+            'type' => 'input',
+            'default_value' => 3
+        )
+    )
+];
+
+return $schema;
