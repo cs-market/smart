7168a80006e6937e11e84d8398d8daa396b50100
bizon <bizon.ru@gmail.com>
2022-01-10 17:27:06 +0300

    [*] new design

--- app/addons/rumba/func.php
+++ app/addons/rumba/func.php
@@ -13,8 +13,13 @@ function fn_get_mobile_app_links() {
 
 function fn_blocks_rumba_get_vendor_info() {
     $company_id = !empty(Tygh::$app['session']['auth']['company_id']) ? Tygh::$app['session']['auth']['company_id'] : null;
+
     $company_data = [];
     $company_data['logos'] = fn_get_logos($company_id);
+
+    if (!is_file($company_data['logos']['theme']['image']['absolute_path'])) {
+        $company_data['logos'] = fn_get_logos(null);
+    }
 
     return $company_data;
 }
