125dcc5fdf77fd66128e3f098fb445ce4979acbf
bizon <bizon.ru@gmail.com>
2022-03-16 21:47:38 +0300

    [!] import order items via product code

--- app/addons/smart_distribution/schemas/exim/order_items.functions.php
+++ app/addons/smart_distribution/schemas/exim/order_items.functions.php
@@ -1,6 +1,7 @@
 <?php
 
 function fn_import_order_items_fill_product_id(&$primary_object_id, &$object, &$pattern, &$options, &$processed_data, &$processing_groups, &$skip_record) {
+    if (!empty($primary_object_id)) return;
     if ($id = db_get_field('SELECT product_id FROM ?:products LEFT JOIN ?:orders ON ?:orders.company_id = ?:products.company_id WHERE ?:orders.order_id = ?i and ?:products.product_code = ?s', $object['order_id'], $object['product_code'])) {
         $object['item_id'] = $object['product_id'] = $id;
     }
