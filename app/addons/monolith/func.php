3f1643fc55b8d31077b85298d6d9b0e7449523bf
bizon <bizon.ru@gmail.com>
2022-06-28 17:41:07 +0300

    [!] monolith combine CRMOrderParam into one

--- app/addons/monolith/func.php
+++ app/addons/monolith/func.php
@@ -6,27 +6,27 @@ use Tygh\Http;
 if (!defined('BOOTSTRAP')) { die('Access denied'); }
 
 function fn_monolith_generate_xml($order_id) {
-	$schema = fn_get_schema('monolith', 'schema');
-	$addon = Registry::get('addons.monolith');
-	$allowed_companies = explode(',', $addon['company_ids']);
+    $schema = fn_get_schema('monolith', 'schema');
+    $addon = Registry::get('addons.monolith');
+    $allowed_companies = explode(',', $addon['company_ids']);
 
-	$order_info = fn_get_order_info($order_id);
-	if (!in_array($order_info['company_id'], $allowed_companies)) {
-		return false;
-	}
+    $order_info = fn_get_order_info($order_id);
+    if (!in_array($order_info['company_id'], $allowed_companies)) {
+        return false;
+    }
 
-	$monolith_order = &$schema['extdata']['scheme']['data']['o'];
-	$monolith_order['d'][] = array(
-		'@attributes' => array('name' => 'CRMOrderParam'),
-		'r' => array(
-			'f' => array(
-				date("Y-m-d\T00:00:00", $order_info['timestamp']),
-				1,
-				1,
-				date("Y-m-d\T00:00:00", $order_info['delivery_date']),
-			),
-		),
-	);
+    $monolith_order = &$schema['extdata']['scheme']['data']['o'];
+    $monolith_order['d'][] = array(
+        '@attributes' => array('name' => 'CRMOrderParam'),
+        'r' => array(
+            'f' => array(
+                date("Y-m-d\T00:00:00", $order_info['timestamp']),
+                1,
+                1,
+                date("Y-m-d\T00:00:00", $order_info['delivery_date']),
+            ),
+        ),
+    );
 
     $d_record = [
         $addon['order_prefix'] . $order_id,
@@ -42,34 +42,36 @@ function fn_monolith_generate_xml($order_id) {
 
     fn_set_hook('monolith_generate_xml', $order_info, $monolith_order, $d_record);
 
-	$monolith_order['d'][] = array(
-		'@attributes' => array ('name' => 'CRMOrder'),
-		'r' => array(
-			'f' => $d_record
-		),
-	);
+    $monolith_order['d'][] = array(
+        '@attributes' => array ('name' => 'CRMOrder'),
+        'r' => array(
+            'f' => $d_record
+        ),
+    );
 
-	$iterator = 0;
-	foreach ($order_info['products'] as $p) {
-		$iterator += 1;
-		$monolith_order['d'][] = array(
-			'@attributes' => array ('name' => 'CRMOrderLine'),
-			'r' => array(
-				'f' => array(
-					$addon['order_prefix'] . $order_id,
-					$iterator,
-					// 'smart.wh1',
-					// '',
-					$p['product_code'],
-					'ea',
-					$p['price'],
-					$p['amount']
-				),
-			),
-		);
-	}
+    $iterator = 0;
+    foreach ($order_info['products'] as $p) {
+        $iterator += 1;
+        $monolith_order['d'][] = array(
+            '@attributes' => array ('name' => 'CRMOrderLine'),
+            'r' => array(
+                'f' => array(
+                    $addon['order_prefix'] . $order_id,
+                    $iterator,
+                    // 'smart.wh1',
+                    // '',
+                    $p['product_code'],
+                    'ea',
+                    $p['price'],
+                    $p['amount']
+                ),
+            ),
+        );
+    }
 
-	if (!empty($order_info['promotions'])) {
+    $CRMOrderOption = [];
+
+    if (!empty($order_info['promotions'])) {
         $promotion_products = array_filter($order_info['products'], function($v) {return (isset($v['promotions']));});
         $applied_promotions = [];
         foreach ($promotion_products as $product) {
@@ -113,38 +115,42 @@ function fn_monolith_generate_xml($order_id) {
                 ),
             );
         }
+
         foreach($applied_promotions as $promotion_id => $promotion) {
             if (empty($promotion['external_id'])) continue;
-            $monolith_order['d'][] = array(
-                '@attributes' => array ('name' => 'CRMOrderOption'),
-                'r' => array(
-                    'f' => array(
-                        $addon['order_prefix'] . $order_id, 
-                        'PrZakaz', 
-                        $promotion['external_id'],
-                    )
-                ),
-            );
+            $CRMOrderOption[] = [
+                'f' => array(
+                    $addon['order_prefix'] . $order_id, 
+                    'PrZakaz', 
+                    $promotion['external_id'],
+                )
+            ];
         }
-	} else {
+    } else {
         unset($schema['extdata']['scheme']['data']['s']['d'][4]);
     }
 
-	// if we have comment
-	if (!empty($order_info['notes'])) {
-		$monolith_order['d'][] = array(
-			'@attributes' => array ('name' => 'CRMOrderOption'),
-			'r' => array(
-				'f' => array(
-					$addon['order_prefix'] . $order_id, 'Comment', $order_info['notes'],
-				)
-			),
-		);
-	} else {
-		unset($schema['extdata']['scheme']['data']['s']['d'][3]);
-	}
+    // if we have comment
+    if (!empty($order_info['notes'])) {
+        $CRMOrderOption[] = [
+            'f' => array(
+                $addon['order_prefix'] . $order_id, 
+                'Comment', 
+                $order_info['notes'],
+            )
+        ];
+    }
 
-	return fn_render_xml_from_array($schema);
+    if (!empty($CRMOrderOption)) {
+        $monolith_order['d'][] = array(
+            '@attributes' => array ('name' => 'CRMOrderOption'),
+            'r' => $CRMOrderOption,
+        );
+    } else {
+        unset($schema['extdata']['scheme']['data']['s']['d'][3]);
+    }
+
+    return fn_render_xml_from_array($schema);
 }
 
 function fn_monolith_send_xml($xml) {
@@ -158,18 +164,18 @@ xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
 <Request xmlns="http://www.monolit.com/xDataLink/">
 <XMLData>
 EOT;
-	$soap .= fn_html_escape($xml);
-	$soap .= <<<EOT
+    $soap .= fn_html_escape($xml);
+    $soap .= <<<EOT
 </XMLData>
 </Request>
 </soap12:Body>
 </soap12:Envelope>
 EOT;
 
-	$result = HTTP::POST($addon['environment_url'], $soap, array(
-		'headers' => array(
-			'Content-type: application/soap+xml; charset=utf-8'
-		))
-	);
-	return $result;
+    $result = HTTP::POST($addon['environment_url'], $soap, array(
+        'headers' => array(
+            'Content-type: application/soap+xml; charset=utf-8'
+        ))
+    );
+    return $result;
 }
