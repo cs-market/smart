2ac585d028b33a538848fb65b3bd66fa97ec9209
bizon <bizon.ru@gmail.com>
2022-08-24 20:10:08 +0300

    [!] fix participates in promo only for cart promotions

--- app/addons/category_promotion/func.php
+++ app/addons/category_promotion/func.php
@@ -339,7 +339,7 @@ function fn_category_promotion_calculate_cart_post($cart, $auth, $calculate_ship
 
     $applied_promotions = array_keys($cart['applied_promotions']);
     foreach ($cart_products as &$product) {
-        list($promotions, ) = fn_get_promotions(['product_or_bonus_product' => $product['product_id'], 'usergroup_ids' => $auth['usergroup_ids'], 'active' => true, 'track' => true, 'exclude_promotion_ids' => $applied_promotions], 10);
+        list($promotions, ) = fn_get_promotions(['product_or_bonus_product' => $product['product_id'], 'zone' => 'cart', 'usergroup_ids' => $auth['usergroup_ids'], 'active' => true, 'track' => true, 'exclude_promotion_ids' => $applied_promotions], 10);
         if ($promotions) {
             $product['participates_in_promo'] = reset($promotions);
         }
