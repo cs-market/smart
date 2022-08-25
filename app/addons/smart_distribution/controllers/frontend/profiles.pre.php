19fc0d8d17abacd7704e91a9f64a2a3b21cdc689
bizon <bizon.ru@gmail.com>
2022-08-25 11:31:31 +0300

    [*] remove delete profile button

--- app/addons/smart_distribution/controllers/frontend/profiles.pre.php
+++ app/addons/smart_distribution/controllers/frontend/profiles.pre.php
@@ -3,11 +3,6 @@
 if (!defined('BOOTSTRAP')) { die('Access denied'); }
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    if ($mode == 'delete_profile') {
-        db_query('UPDATE ?:users SET password = ?s WHERE user_id = ?i', fn_generate_password(4), $auth['user_id']);
-        fn_user_logout($auth);
-        fn_set_notification('N', __('notice'), __('text_profile_deleted'));
-    }
 	return;
 }
 
