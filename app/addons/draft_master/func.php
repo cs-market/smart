30ce6d78bf21dfe54a1d975a8c7bbe010cf9959d
bizon <bizon.ru@gmail.com>
2022-10-14 20:52:04 +0300

    [+] order reviews for api

--- app/addons/draft_master/func.php
+++ app/addons/draft_master/func.php
@@ -9,7 +9,7 @@ function fn_draft_master_get_user_info(&$user_id, &$get_profile, &$profile_id, &
 }
 
 function fn_draft_master_user_init($auth, &$user_info) {
-    if (empty($user_info['extra_data'])) {
+    if (empty($user_info['extra_data']) && !empty($user_info['user_id'])) {
         $user_info['extra_data'] = fn_get_user_additional_data(USER_EXTRA_DATA, $user_info['user_id']);
     }
 }
