bad8f05b721d24275b8063695f095f1cd4169362
bizon <bizon.ru@gmail.com>
2022-11-28 17:49:20 +0300

    [*] redirect to order details

--- app/addons/smart_distribution/controllers/frontend/checkout.pre.php
+++ app/addons/smart_distribution/controllers/frontend/checkout.pre.php
@@ -5,8 +5,12 @@ if (!defined('BOOTSTRAP')) { die('Access denied'); }
 $cart = & Tygh::$app['session']['cart'];
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-	if ($mode == 'add' && $action == 'wishlist') {
-		$_REQUEST['product_data'] = Tygh::$app['session']['wishlist']['products'];
-	}
-	return array(CONTROLLER_STATUS_OK);
+    if ($mode == 'add' && $action == 'wishlist') {
+        $_REQUEST['product_data'] = Tygh::$app['session']['wishlist']['products'];
+    }
+    return array(CONTROLLER_STATUS_OK);
+}
+
+if ($mode == 'complete' && !empty($_REQUEST['order_id'])) {
+    return array(CONTROLLER_STATUS_REDIRECT, 'orders.details?order_id='.$_REQUEST['order_id']);
 }
