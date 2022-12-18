1df9996bafb92db16bee57713bd6434101bb49a8
bizon <bizon.ru@gmail.com>
2022-12-18 14:53:29 +0300

    [*] export storage code in order

---
+++ app/addons/storages/schemas/exim/orders_with_items.post.php
@@ -0,0 +1,18 @@
+<?php
+
+$schema['export_fields']['Storage name'] = array (
+    'db_field' => 'storage',
+    'table' => 'storages',
+    'linked' => true,
+);
+$schema['export_fields']['Storage code'] = array (
+    'db_field' => 'code',
+    'table' => 'storages',
+    'linked' => true,
+);
+$schema['references']['storages'] = [
+    'reference_fields' => ['storage_id' => '#orders.storage_id'],
+    'join_type'        => 'LEFT',
+];
+
+return $schema;
