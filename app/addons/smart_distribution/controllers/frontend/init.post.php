714ae5a5fa7de01d2c19bc075981ecebcd39bdec
bizon <bizon.ru@gmail.com>
2022-12-27 23:13:14 +0300

    [*] disable any notifications for beersib

--- app/addons/smart_distribution/controllers/frontend/init.post.php
+++ app/addons/smart_distribution/controllers/frontend/init.post.php
@@ -5,4 +5,4 @@ if (AREA == 'C' && empty($auth['user_id']) && in_array($controller, array('index
 	fn_redirect('auth.login_form');
 }
 
-if (in_array($auth['company_id'], [1790])) Tygh::$app['session']['notifications'] = [];
+if (in_array($auth['company_id'], [2186])) Tygh::$app['session']['notifications'] = [];
