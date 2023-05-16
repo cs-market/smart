fc17167257ba5f18288d96d6b7b75316f555d37f
bizon <bizon.ru@gmail.com>
2023-05-16 17:36:26 +0300

    [+] import user extra data

--- app/addons/draft_master/func.php
+++ app/addons/draft_master/func.php
@@ -5,3 +5,9 @@ if (!defined('BOOTSTRAP')) { die('Access denied'); }
 function fn_get_extra_user_data() {
     return fn_get_user_additional_data(USER_EXTRA_DATA, Tygh::$app['session']['auth']['user_id']);
 }
+
+function fn_draft_master_update_profile($action, $user_data, $current_user_data) {
+    if (!empty($user_data['extra_data'])) {
+        fn_save_user_additional_data(USER_EXTRA_DATA, $user_data['extra_data'], $user_data['user_id']);
+    }
+}
--- app/addons/draft_master/init.php
+++ app/addons/draft_master/init.php
@@ -5,6 +5,7 @@ if (!defined('BOOTSTRAP')) { die('Access denied'); }
 define('USER_EXTRA_DATA', 'E');
 
 fn_register_hooks(
+    'update_profile'
     // 'get_user_info',
     // 'user_init'
 );
