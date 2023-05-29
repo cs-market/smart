dbce4fedb629d37d3da6e14d326c26846547698b
bizon <bizon.ru@gmail.com>
2023-05-29 15:48:36 +0300

    [*] extended reward points

--- app/addons/extended_reward_points/func.php
+++ app/addons/extended_reward_points/func.php
@@ -307,23 +307,23 @@ function fn_generate_reward_points_report($params) {
 
     $output = array();
     if (isset($params['is_search'])) {
-
         list($orders, $c_params, $totals) = fn_get_orders($params);
+
+        $ttl = db_get_hash_single_array('SELECT ttl, order_id FROM ?:reward_point_details WHERE order_id IN (?a)', array('order_id', 'ttl'), array_column($orders, 'order_id'));
+        $points_info = db_get_hash_single_array('SELECT data, order_id FROM ?:order_data WHERE order_id IN (?a) AND type = ?s', array('order_id', 'data'), array_column($orders, 'order_id'), POINTS_IN_USE);
+        $users_data = db_get_hash_array('SELECT u.user_id, u.user_login, p.s_state, d.data FROM ?:users AS u LEFT JOIN ?:user_profiles AS p ON u.user_id = p.user_id LEFT JOIN ?:user_data AS d ON d.user_id = u.user_id AND d.type = ?s WHERE u.user_id IN (?a)', 'user_id', POINTS, array_column($orders, 'user_id'));
 
         if ($orders) {
             foreach ($orders as $order) {
-                $user = fn_get_user_info($order['user_id']);
-                $points_info = unserialize(db_get_field('SELECT data FROM ?:order_data WHERE order_id = ?i AND type = ?s', $order['order_id'], POINTS_IN_USE));
-                $ttl = db_get_field('SELECT ttl FROM ?:reward_point_details WHERE order_id = ?i', $order['order_id']);
                 $output[] = [
-                    __('region') => $user['s_state'],
-                    __('login') => $user['user_login'],
+                    __('region') => $users_data[$order['user_id']]['s_state'],
+                    __('login') => $users_data[$order['user_id']]['user_login'],
                     __('order') => $order['order_id'],
                     __('order_date') => fn_date_format($order['timestamp'], Registry::get('settings.Appearance.date_format')),
                     __('earned_points') => $order['points'] ?? 0,
-                    __('extended_reward_points.reward_points_ttl') => (!empty($ttl)) ? fn_date_format($ttl, Registry::get('settings.Appearance.date_format')) : '', 
-                    __('points_in_use') => $points_info['points'] ?? 0,
-                    __('extended_reward_points.user_points') => fn_get_user_additional_data(POINTS, $order['user_id'])
+                    __('extended_reward_points.reward_points_ttl') => (!empty($ttl[$order['order_id']])) ? fn_date_format($ttl[$order['order_id']], Registry::get('settings.Appearance.date_format')) : '-',
+                    __('points_in_use') => (!empty($points_info[$order['order_id']])) ? unserialize($points_info[$order['order_id']])['points'] : 0,
+                    __('extended_reward_points.user_points') => (!empty($users_data[$order['user_id']]['data'])) ? unserialize($users_data[$order['user_id']]['data']) : 0
                 ];
             }
         }
