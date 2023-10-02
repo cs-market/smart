e42bc17a4fefb9733f246cf248b08cb249098d7b
bizon <bizon.ru@gmail.com>
2023-10-02 18:36:20 +0300

    [+] allow vendors to change user reward points

---
+++ app/addons/extended_reward_points/schemas/permissions/vendor_multivendor.post.php
@@ -0,0 +1,7 @@
+<?php
+
+$schema['controllers']['reward_points']['modes']['userlog']['permissions'] = true;
+$schema['controllers']['reward_points']['modes']['change_points']['permissions'] = true;
+$schema['controllers']['reward_points']['modes']['m_delete']['permissions'] = true;
+
+return $schema;
--- app/addons/maintenance/schemas/permissions/vendor_multivendor.post.php
+++ app/addons/maintenance/schemas/permissions/vendor_multivendor.post.php
@@ -2,6 +2,5 @@
 
 $schema['controllers']['profiles']['modes']['act_as_user']['permissions'] = true;
 $schema['controllers']['cart']['permissions'] = true;
-$schema['controllers']['reward_points']['modes']['userlog']['permissions'] = true;
 
 return $schema;
