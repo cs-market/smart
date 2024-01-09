d74498466e565e37ad79f4cef330f131ceddfee4
bizon <bizon.ru@gmail.com>
2024-01-09 16:51:18 +0300

    [*] balt custom registration

--- app/addons/monolith/controllers/frontend/auth.post.php
+++ app/addons/monolith/controllers/frontend/auth.post.php
@@ -8,6 +8,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     return;
 }
 
+if (Registry::ifGet('runtime.shop_id', 0) == 2 && empty($auth['user_id'])) {
+    $_SESSION['custom_registration'] = 45;
+}
+
 if ($mode == 'login_form' && Registry::ifGet('runtime.shop_id', 0) == 2 && fn_allowed_for('MULTIVENDOR')) {
     return [CONTROLLER_STATUS_REDIRECT, 'auth.baltika_login_form'];
 }
