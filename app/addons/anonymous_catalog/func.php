2774c6ca27afb61afadf5174d9dad00b05be014b
bizon <bizon.ru@gmail.com>
2024-03-20 20:20:02 +0300

    [*] baltika auth page addon

--- app/addons/anonymous_catalog/func.php
+++ app/addons/anonymous_catalog/func.php
@@ -10,5 +10,7 @@ function fn_anonymous_catalog_get_products($params, $fields, $sortings, &$condit
     if (Registry::get('runtime.mode') != 'product_catalog' || $params['area'] != 'C' || $auth['user_id']) return;
 
     $search_condition = ' AND (' . fn_find_array_in_set($auth['usergroup_ids'], 'products.usergroup_ids', true) . ')';
-    $condition = str_replace($search_condition, '', $condition);
+
+    $add_condition = ' AND (' . fn_find_array_in_set(explode(',',Registry::get('addons.anonymous_catalog.anonymous_usergroups')), 'products.usergroup_ids', true) . ')';
+    $condition = str_replace($search_condition, $add_condition, $condition);
 }
