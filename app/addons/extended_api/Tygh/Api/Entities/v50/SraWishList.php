1703ca11195d853a480e879e4366cf5c78b275e4
bizon <bizon.ru@gmail.com>
2024-05-09 16:37:01 +0300

    [!] fix prices in wishlist with catalog promotions for application

--- app/addons/extended_api/Tygh/Api/Entities/v50/SraWishList.php
+++ app/addons/extended_api/Tygh/Api/Entities/v50/SraWishList.php
@@ -36,6 +36,7 @@ class SraWishList extends BaseSraWishList
             );
             foreach($response['data']['products'] as &$product) {
                 $product = fn_get_product_data($product['product_id'], $this->auth, CART_LANGUAGE, '', true, true, true, false, false, true, false, true);
+                fn_gather_additional_product_data($product);
                 foreach ($fields as $field) {
                     if (isset($product[$field])) {
                         $product[$field] = fn_format_price($product[$field], CART_PRIMARY_CURRENCY, null, false);
@@ -45,5 +46,30 @@ class SraWishList extends BaseSraWishList
         }
 
         return $response;
+    }
+
+
+    /**
+     * Calculates cart content with promotions, taxes and shipping.
+     *
+     * @param array<string, int|string|array> $cart      Cart data
+     * @param array<string, int|string|array> $params    Calculation parameters
+     * @param string                          $lang_code Two-letter language code
+     *
+     * @return array<string, int|string|array>
+     */
+    protected function calculate(array $cart, array $params = [], $lang_code = DEFAULT_LANGUAGE)
+    {
+        $cart = $this->setUserInfo($cart, fn_get_user_info($this->auth['user_id']));
+        $cart['default_location'] = $this->getDefaultLocation($lang_code);
+
+        // add payment methods
+        $cart['payments'] = $this->getPayments($cart, $lang_code);
+
+        // remove sensitive and redundant information
+        $cart = fn_storefront_rest_api_strip_service_data($cart);
+
+
+        return $cart;
     }
 }
